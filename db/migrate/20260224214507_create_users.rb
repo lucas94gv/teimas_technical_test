98ac3b80f06814fd137b634ad288d278a86db8a0
Lucas García Viz <lucas94gv@gmail.com>
2026-02-25 12:20:45 +0100

    users migration and model

---
+++ db/migrate/20260224214507_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def up
+    create_table :users, id: :uuid do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+
+  def down
+    drop_table :users
+  end
+end
