64bd885b3b748c97174ef8b60be33ca149548c05
Lucas García Viz <lucas94gv@gmail.com>
2026-02-26 19:50:16 +0100

    funcionality fixtures

--- app/controllers/api/v1/fixtures_controller.rb
+++ app/controllers/api/v1/fixtures_controller.rb
@@ -2,12 +2,17 @@ module Api::V1
   class FixturesController < ApplicationController
     before_action :authorize_request
 
-    # GET /api/v1/fixtures
+    # GET /api/v1/fixtures?team_id=529
     def index
-      team_ids = current_user.user_teams.pluck(:team_id)
+      team_ids = if params[:team_id].present?
+                   [params[:team_id].to_i]
+                 else
+                   current_user.user_teams.pluck(:team_id)
+                 end
+
       fixtures = team_ids.flat_map do |team_id|
         ApiFootballService.team_fixtures(team_id, timezone: 'Europe/London', season: 2024)
-      end
+      end.uniq { |f| f["fixture"]["id"] }
 
       fixtures.sort_by! { |f| f["fixture"]["date"] }
 
